21bd82887b630d91e051327dd1aba50b280a2875
jude <jude.niroshan11@gmail.com>
2022-09-27 17:14:41 +0200

    scc changes for service accounts

--- tenant-manager-backend/src/main/resources/boutique_files/create-namespace.sh
+++ tenant-manager-backend/src/main/resources/boutique_files/create-namespace.sh
@@ -8,44 +8,79 @@ export NAMESPACE
 export PWD
 #
 # Check if the namespace already exists
-#if oc get namespace -o json | jq -r ".items[].metadata.name" | grep -w $NAMESPACE; then \
+#if oc get namespace -o json | jq -r ".items[].metadata.name" | grep $NAMESPACE; then \
 #  echo "The Namespace $NAMESPACE already exists"
 #else
-  echo "Starting to provision OCP resources..."
-  # Create the Namespace
-#  echo "apiVersion: v1 \
-#  kind: Namespace \
-#  metadata: \
-#    name: ${NAMESPACE}" | oc create -f -
-
-  oc create ns $NAMESPACE
-
-
-  # Modify privileges for the default service account in scc. This step needs to be reviewed as the gives the service account too much privileges.
-  oc adm policy add-scc-to-user privileged -z default -n $1
-  #
-  # Change into the new Namespace
-  oc project ${NAMESPACE}
-  #
-  # Deploy the all-in-one application stack
-  oc apply -f ${PWD}/all-in-one.yaml
-  #
-  # **Need to create logic to monitor the website until the service is up and running**
-  #
-  # Expose the frontend service
-  oc expose svc frontend --name=$1-route
-  #--hostname=$2.pebcac.org
-  #
-  # Get the url for the website
-  ROUTE=`oc get route | cut -d" " -f4 | xargs`
-  #for i in `curl -kvv $ROUTE`; do grep "HTTP\/1.1 200"
-  #
-  # Apply a quota to the namespace
-  oc apply -f ${PWD}/boutique-quota.yaml
-  #
-  # Apply autoscaling for the frontend service
-  oc apply -f ${PWD}/frontend-hpa.yaml
-
-  echo "The shop url is "http://${ROUTE}""
-
+#
+# Create the Namespace
+#echo "apiVersion: v1
+#kind: Namespace
+#metadata:
+#  name: ${NAMESPACE}" | oc apply -f -
 #fi
+oc create ns $NAMESPACE
+#
+# Modify privileges for the default service account in scc. This step needs to be reviewed as the gives the service account too much privileges.
+oc create sa tenant-controller-sa -n boutique
+
+oc adm policy add-scc-to-user privileged -z default -n boutique
+oc adm policy add-scc-to-user privileged -z default -n saas-boutique
+oc adm policy add-scc-to-user privileged -z tenant-controller-sa -n saas-boutique
+oc adm policy add-scc-to-user privileged -z tenant-controller-sa -n boutique
+#
+# Change into the new Namespace
+oc project ${NAMESPACE}
+#
+# Deploy the all-in-one application stack
+oc apply -f ${PWD}/all-in-one.yaml
+#
+# **Need to create logic to monitor the website until the service is up and running**
+#
+# Expose the frontend service
+oc expose svc frontend --name=$1-route # --hostname=$2.pebcac.org
+#
+# Sleep statement to allow for the frontend service to come online
+sleep 10
+#
+# Get the url for the website
+ROUTE=`oc get route | cut -d" " -f4 | xargs`
+#for i in `curl -kvv $ROUTE`; do grep "HTTP\/1.1 200"
+#
+# Apply a quota to the namespace
+oc apply -f ${PWD}/boutique-quota.yaml
+#
+# Apply autoscaling for the frontend service
+oc apply -f ${PWD}/autoscaler/frontend-hpa.yaml
+
+# Apply autoscaling for the addservice service
+oc apply -f ${PWD}/autoscaler/adservice-hpa.yaml
+
+# Apply autoscaling for the cart service
+oc apply -f ${PWD}/autoscaler/cartservice-hpa.yaml
+
+# Apply autoscaling for the currency service
+oc apply -f ${PWD}/autoscaler/currencyservice-hpa.yaml
+
+# Apply autoscaling for the email service
+oc apply -f ${PWD}/autoscaler/emailservice-hpa.yaml
+
+# Apply autoscaling for the payment service
+oc apply -f ${PWD}/autoscaler/paymentservice-hpa.yaml
+
+# Apply autoscaling for the checkout service
+oc apply -f ${PWD}/autoscaler/checkoutservice-hpa.yaml
+
+# Apply autoscaling for the productcatalog service
+oc apply -f ${PWD}/autoscaler/productcatalogservice-hpa.yaml
+
+# Apply autoscaling for the redis service
+oc apply -f ${PWD}/autoscaler/redisservice-hpa.yaml
+
+# Apply autoscaling for the recommendation service
+oc apply -f ${PWD}/autoscaler/recommendservice-hpa.yaml
+
+# Apply autoscaling for the shipping service
+oc apply -f ${PWD}/autoscaler/shippingservice-hpa.yaml
+
+# Validation of the route
+echo "The shop url is "http://${ROUTE}""
