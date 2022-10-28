be6419183f62fccc5ac1de4fcff0e5b849c54ae8
Preston Davis <pdavis@redhat.com>
2022-10-28 08:45:29 -0400

    Removed the olm check for testing

--- tenant-manager-operator/operator-build.sh
+++ tenant-manager-operator/operator-build.sh
@@ -4,24 +4,18 @@ set -e
 set -o pipefail
 
 HELM_REPO=https://rhecosystemappeng.github.io/saas-ecommerce-boutique-shop
-HELM_CHART=rh-saas/tenant-manager-chart
+HELM_CHART=tenant-manager-chart
 CLUSTER_DOMAIN=$(oc get ingresses.config.openshift.io cluster -ojsonpath='{.spec.domain}')
 
 # Returns 0 if the OLM status is missing, 1 otherwise
 function checkOlmStatus() {
-  log "Checking OLM status"
   systemControllerOperator=$(operator-sdk olm status)
   if [ ! -z "$systemControllerOperator" ]; then
-    log "The OLM is not responding. Please validate that you are connected to and OpenShift cluster."
     return 1
   else
     return 0
   fi
 }
-
-# Connecto the helm repo and install the chart
-#helm repo add rh-saas $HELM_REPO
-#helm install demo-release $HELM_CHART  #May not need to call this line since it is called later in the script
 
 # Stage the operator
 operator-sdk init --verbose --plugins=helm.sdk.operatorframework.io/v1 \
@@ -31,11 +25,11 @@ operator-sdk init --verbose --plugins=helm.sdk.operatorframework.io/v1 \
   --helm-chart-repo=$HELM_REPO
 
 # Update the operator CRDs and OLM to the latest version
-oc apply -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.22.0/crds.yaml
-oc apply -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.22.0/olm.yaml
+#oc apply -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.22.0/crds.yaml
+#oc apply -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.22.0/olm.yaml
 
 # Validate OLM status
-checkOlmStatus
+#checkOlmStatus
 
 # Make the operator image
 make docker-build docker-push IMG="quay.io/ecosystem-appeng/saas-tenant-operator:v0.0.1"
