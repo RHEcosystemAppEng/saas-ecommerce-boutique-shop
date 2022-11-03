dd7e6c1153abd768958e12db55a0f4d09f461de0
jude <jude.niroshan11@gmail.com>
2022-11-03 15:34:32 +0530

    Fix private route issue

--- tenant-manager-frontend/src/Components/util/PrivateRoutes.js
+++ tenant-manager-frontend/src/Components/util/PrivateRoutes.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {Outlet, Navigate} from "react-router-dom";
 
 const PrivateRoutes = () => {
-    let auth = {'token': localStorage.getItem('tenantKey') !== undefined}
+    let auth = {'token': localStorage.getItem('tenantKey') !== null &&
+            localStorage.getItem('tenantKey') !== undefined}
     return (
         auth.token ? <Outlet/> : <Navigate to='/login'/>
     )
