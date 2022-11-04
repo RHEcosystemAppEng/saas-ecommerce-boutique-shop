30b4fdca04fb50d337485126933d5e7356d4a052
asaurabh74 <asaurabh@yahoo.com>
2022-11-04 11:02:21 -0400

    Update operator-build.sh

--- tenant-manager-operator/operator-build.sh
+++ tenant-manager-operator/operator-build.sh
@@ -3,24 +3,30 @@
 set -e
 set -o pipefail
 
+rm -rf bin bundle config helm-charts bundle.Dockerfile Dockerfile Makefile PROJECT watches.yaml
+oc delete ns boutique-operator-demo || true
+
 HELM_REPO=https://rhecosystemappeng.github.io/saas-ecommerce-boutique-shop
 HELM_CHART=tenant-manager-chart
-CLUSTER_DOMAIN=$(oc get ingresses.config.openshift.io cluster -ojsonpath='{.spec.domain}')
+CLUSTER_DOMAIN=apps.ocp.pebcac.org
+IMAGE_TAG_BASE="quay.io/ecosystem-appeng/saas-tenant-operator"
+VERSION=v0.0.1
+IMG="$IMAGE_TAG_BASE:$VERSION"
+BUNDLE_IMG="$IMAGE_TAG_BASE-bundle:$VERSION"
 
 # Returns 0 if the OLM status is missing, 1 otherwise
-function checkOlmStatus() {
-  systemControllerOperator=$(operator-sdk olm status)
-  if [ ! -z "$systemControllerOperator" ]; then
-    return 1
-  else
-    return 0
-  fi
-}
+#function checkOlmStatus() {
+#  systemControllerOperator=$(operator-sdk olm status)
+#  if [ ! -z "$systemControllerOperator" ]; then
+#    return 1
+#  else
+#    return 0
+#  fi
+#}
 
 # Stage the operator
 operator-sdk init --verbose --plugins=helm.sdk.operatorframework.io/v1 \
   --domain=$CLUSTER_DOMAIN \
-  --project-name=boutique-demo \
   --helm-chart=$HELM_CHART \
   --helm-chart-repo=$HELM_REPO
 
@@ -32,13 +38,13 @@ operator-sdk init --verbose --plugins=helm.sdk.operatorframework.io/v1 \
 #checkOlmStatus
 
 # Make the operator image
-make docker-build docker-push IMG="quay.io/ecosystem-appeng/saas-tenant-operator:v0.0.1"
+make docker-build docker-push IMG="$IMG"
 
 # Make the operator bundle by adding OLM to the operator image
-make bundle IMG="quay.io/ecosystem-appeng/saas-tenant-operator:v0.0.1"
-make bundle-build bundle-push IMG="quay.io/ecosystem-appeng/saas-tenant-operator:v0.0.1"
+make bundle IMG="$IMG" IMAGE_TAG_BASE="$IMAGE_TAG_BASE"
+make bundle-build bundle-push IMG="$IMG" IMAGE_TAG_BASE="$IMAGE_TAG_BASE"
 
 # Make a new namespace and deploy the operator
 oc new-project boutique-operator-demo
-operator-sdk run bundle quay.io/ecosystem-appeng/saas-tenant-operator:v0.0.1
+operator-sdk run bundle $BUNDLE_IMG
 
