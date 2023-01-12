92f84e1ac8bf892477109b9cdd4f9b9e217400a4
Saurabh Agarwal <sauagarw@redhat.com>
2023-01-12 08:25:33 -0500

    Add support for HPA CR

--- boutique-shop-operator/main.go
+++ boutique-shop-operator/main.go
@@ -169,6 +169,13 @@ var rootCmd = &cobra.Command{
 			setupLog.Error(err, "unable to create controller", "controller", "BoutiqueShop")
 			os.Exit(1)
 		}
+		if err = (&controllers.HpaScalerReconciler{
+			Client: mgr.GetClient(),
+			Scheme: mgr.GetScheme(),
+		}).SetupWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create controller", "controller", "HpaScaler")
+			os.Exit(1)
+		}
 		//+kubebuilder:scaffold:builder
 
 		if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
