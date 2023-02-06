11d43d769741535b164b80160d8b77876c18e295
Saurabh Agarwal <sauagarw@redhat.com>
2023-02-05 21:46:27 -0500

    Added quote in the script

--- tenant-manager-chart/boutique_files/create-hpa-cr.sh
+++ tenant-manager-chart/boutique_files/create-hpa-cr.sh
@@ -5,4 +5,4 @@ TIER=$(echo "$1" | tr '[:upper:]' '[:lower:]') ## Tier selection - Ensure it's a
 RULES_RESPONSE_PAYLOAD=$(echo "$2" | tr '[:upper:]' '[:lower:]')  ## This is the rules engine response json payload specific to a given tier
 
 echo "Applying HPA scaler CR for the Tier: $1"
-oc process -f ${SCRIPT_DIR}/boutique-hpa-scaler.yaml --param=TIER=$1 --param=RULES_RESPONSE_PAYLOAD=$2 | oc apply -f -
+oc process -f ${SCRIPT_DIR}/boutique-hpa-scaler.yaml --param=TIER=$1 --param=RULES_RESPONSE_PAYLOAD="$2" | oc apply -f -
